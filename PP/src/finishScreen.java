3ac5572529d4cd3defc07762a4f42d55f7c24fd9
austy <austy@10.129.229.87>
2023-03-23 10:09:16 -0400

    started stuff

--- PP/src/finishScreen.java
+++ PP/src/finishScreen.java
@@ -1,4 +1,14 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class finishScreen {
+public class finishScreen implements ActionListener {
+	
 
 }
--- PP/src/welcomeScreen.java
+++ PP/src/welcomeScreen.java
@@ -1,4 +1,46 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class welcomeScreen {
-
+public class welcomeScreen implements ActionListener {
+	
+	JFrame frame;
+	JPanel panel;
+	JButton ezButt, hardButt;
+	JLabel logo;
+	
+	welcomeScreen(JFrame frame, JPanel panel){
+		this.frame = frame;
+		this.panel = panel;
+		panel.setLayout(null);
+		logo = new JLabel(new ImageIcon(getClass().getClassLoader().getResource("ppLogo.png")));
+	}
+	
+	public void display(JFrame frame, JPanel panel) {
+		
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+	
+//	public static void main(String[]args) {
+//		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//			public void run() {
+//				JFrame.setDefaultLookAndFeelDecorated(true);
+//				
+//				JFrame testFrame = new JFrame();
+//				
+//				
+//				new welcomeScreen().display(new JFrame(), new JPanel());
+//			}
+//		});
+//	}
 }
