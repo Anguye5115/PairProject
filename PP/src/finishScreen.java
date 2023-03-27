753b07ad3d14a11ca7b0bcc45d742f3104c57a8b
austy <austy@10.129.229.80>
2023-03-27 10:05:00 -0400

    finishscreen design complete

--- PP/src/finishScreen.java
+++ PP/src/finishScreen.java
@@ -3,6 +3,8 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,9 +17,16 @@ public class finishScreen implements ActionListener {
 	JPanel contentPane;
 	JButton playAgainButt;
 	JLabel winnerPic;
+	int winIndex;
 	//0 = player, 1 = bot, 2 = tie
 	
-	public void display(/*JFrame frame, gamePanel contentPane,*/ int winIndex) {
+	private boolean signal = false;
+	
+	public void getWinIndex(int num) {
+		winIndex = num;
+	}
+	
+	public void display(/*JFrame frame, gamePanel contentPane*/) {
 //		this.frame = frame;
 //		this.contentPane = contentPane;
 		
@@ -25,21 +34,52 @@ public class finishScreen implements ActionListener {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		contentPane = new JPanel();
 		contentPane.setPreferredSize(new Dimension(500, 750));
-		frame.add(contentPane);
-		frame.pack();
-		frame.setVisible(true);
+		contentPane.setLayout(null);
+		contentPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		
+		playAgainButt = new JButton("Play Again");
+		playAgainButt.setActionCommand("click");
+		playAgainButt.addActionListener(this);
+		playAgainButt.setFont(new Font("Serif", Font.PLAIN, 20));
+		playAgainButt.setBounds(150, 600, 200, 50);
+		contentPane.add(playAgainButt);
 		
 		if (winIndex == 0) {
-			winnerPic = new JLabel(new ImageIcon(getClass().getClassLoader().getResource("youWin.png")));
+			winnerPic = new JLabel(new ImageIcon("images/youWin.png"));
 		} else if (winIndex == 1) {
-			winnerPic = new JLabel(new ImageIcon(getClass().getClassLoader().getResource("youLose.png")));
+			winnerPic = new JLabel(new ImageIcon("images/youLose.png"));
 		} else if (winIndex == 2) {
-			winnerPic = new JLabel(new ImageIcon(getClass().getClassLoader().getResource("ppLogo.png")));
+			winnerPic = new JLabel(new ImageIcon("images/tie.png"));
 		}
 		
-		contentPane.setLayout(null);
+		winnerPic.setBounds(-5, 100, 500, 500);
+		contentPane.add(winnerPic);
 		
-		
+		frame.setContentPane(contentPane);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	//WIP
+	public void hide(JFrame frame, JPanel panel) {
+		panel.remove(playAgainButt);
+		panel.remove(winnerPic);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand() == "click") {
+			signal = true;
+			System.out.println("clicked");
+			hide(frame, contentPane);
+		}
+	}
+	
+	public boolean checkSignal() {
+		return signal;
+	}
+	
+	public void resetSignal() {
+		signal = false;
 	}
 	
 	public static void main(String[] args) {
@@ -47,13 +87,10 @@ public class finishScreen implements ActionListener {
 			public void run() {
 				JFrame.setDefaultLookAndFeelDecorated(true);
 				finishScreen test = new finishScreen();
+				test.getWinIndex(2);
 				test.display();
 			}
 		});
-	}
-
-	public void actionPerformed(ActionEvent e) {
-
 	}
 
 }
--- PP/src/gamePanel.java
+++ PP/src/gamePanel.java
@@ -1,20 +1,20 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Image;
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
-
-
-public class gamePanel extends JPanel {
-	gamePanel(int width, int height) {
-		setPreferredSize(new Dimension(width, height));
-	}
-	
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		if (runGame.state == runGame.gameState.GAMESCREEN) {
-			
-		}
-	}
-}
+//import java.awt.Color;
+//import java.awt.Dimension;
+//import java.awt.Graphics;
+//import java.awt.Image;
+//import javax.swing.ImageIcon;
+//import javax.swing.JPanel;
+//
+//
+//public class gamePanel extends JPanel {
+//	gamePanel(int width, int height) {
+//		setPreferredSize(new Dimension(width, height));
+//	}
+//	
+//	public void paintComponent(Graphics g) {
+//		super.paintComponent(g);
+//		if (runGame.state == runGame.gameState.GAMESCREEN) {
+//			
+//		}
+//	}
+//}
