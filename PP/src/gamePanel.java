60a7d9eb96578c191f27f8e482d78db5c8d727e3
austy <austy@10.129.224.249>
2023-04-26 10:07:15 -0400

    the ball moves

--- PP/src/gamePanel.java
+++ PP/src/gamePanel.java
@@ -1,48 +1,118 @@
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Image;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.util.Timer;
 import java.util.TimerTask;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 
-public class gamePanel extends JPanel {
-	
+public class gamePanel extends JPanel implements KeyListener {
+
 	JFrame frame;
-	JPanel contentPane;
 	
-	gamePanel(/*int width, int height*/) {
-//		setPreferredSize(new Dimension(width, height));
-		
+	int ballx = 375;
+	int bally = 375;
+	int balldx = 3;
+	int balldy = 3;
+	
+	int playerx = 15;
+	int playery = 375;
+	int playerdx, playerdy;
+	
+
+	gamePanel() {
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		contentPane = new JPanel();
-		frame.setContentPane(contentPane);
-		frame.setSize(500, 750);
-		frame.setVisible(true);
 		
+		frame.addKeyListener(this);
+		frame.add(this);
+		frame.setSize(750, 750);
+		frame.setVisible(true);
 	}
-	//testing ni hao
+	
+	public void ballMvt() {
+		int width = getWidth();
+		int height = getHeight();
+
+		ballx = ballx + balldx;
+		bally = bally + balldy;
+		
+		if (ballx < 10) {
+			balldx = -balldx;
+			ballx = 10;
+		} else if ((ballx + 50) > (width - 10)){
+			balldx = -balldx;
+			ballx = width - 60;
+		}
+
+		if (bally < 10) {
+			balldy = -balldy;
+			bally = 10;
+		} else if ((bally + 50) > (height - 5)) {
+			balldy = -balldy;
+			bally = height - 60;
+		}
+	}
+	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
+		//code for the borders
 		g.setColor(Color.BLACK);
-		g.drawLine(10, 10, 480, 10);
-		g.drawLine(480, 10, 480, 730);
-		g.drawLine(480, 730, 10, 730);
-		g.drawLine(10, 730, 10, 10);
+		g.drawLine(10, 10, 730, 10);
+		g.drawLine(730, 10, 730, 710);
+		g.drawLine(730, 710, 10, 710);
+		g.drawLine(10, 710, 10, 10);
 		
+		//code for ball mvt
+		g.setColor(Color.PINK);
+		g.fillOval(ballx, bally, 50, 50);
+		
+		//code for player mvt
+		g.setColor(Color.MAGENTA);
+		g.fillRect(15, 15, 15, 100);
 	}
-
+	
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				JFrame.setDefaultLookAndFeelDecorated(true);
+
 				gamePanel test = new gamePanel();
+				Timer timer = new Timer();
+				TimerTask task = new TimerTask() {
+					public void run() {
+						test.ballMvt();
+						test.repaint();
+						try {
+							Thread.sleep(10);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				};
+				timer.schedule(task, 1, 1);
 			}
 		});
 	}
+
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	public void keyPressed(KeyEvent e) {
+		System.out.println("Testing");
+		switch (e.getKeyChar()) {
+		case KeyEvent.VK_W:
+			System.out.println("W pressed");
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+		
+	}
+	
 }
