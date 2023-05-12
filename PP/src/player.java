a083853ee1fe649b987529819ff62b389b979348
kelseylo <kelseylo326@gmail.com>
2023-05-11 23:19:43 -0400

    player

--- PP/src/player.java
+++ PP/src/player.java
@@ -5,25 +5,7 @@ public class player {
 	int playery = 330;
 	int playerdx, playerdy;
 	
-	player(KeyEvent e){
-		if ((e.getKeyCode() == KeyEvent.VK_W) || (e.getKeyCode() == KeyEvent.VK_UP)) {
-			if (playery > 10) {
-				playery -= 25;	
-			}
-		}
-
-		if ((e.getKeyCode() == KeyEvent.VK_S) || (e.getKeyCode() == KeyEvent.VK_DOWN)) {
-			if (playery < 740) {
-				playery += 25;	
-			}
-		}		
-		
-		if ((e.getKeyCode() == KeyEvent.VK_W) || (e.getKeyCode() == KeyEvent.VK_UP) || (e.getKeyCode() == KeyEvent.VK_S) || (e.getKeyCode() == KeyEvent.VK_DOWN)) {
-			playerdy = 0;
-		}	
-	}
-
-//	public void keyPressed(KeyEvent e) {
+//	player(KeyEvent e){
 //		if ((e.getKeyCode() == KeyEvent.VK_W) || (e.getKeyCode() == KeyEvent.VK_UP)) {
 //			if (playery > 10) {
 //				playery -= 25;	
@@ -34,14 +16,33 @@ public class player {
 //			if (playery < 740) {
 //				playery += 25;	
 //			}
-//		}			
-//	}
-//
-//	public void keyReleased(KeyEvent e) {
+//		}		
+//		
 //		if ((e.getKeyCode() == KeyEvent.VK_W) || (e.getKeyCode() == KeyEvent.VK_UP) || (e.getKeyCode() == KeyEvent.VK_S) || (e.getKeyCode() == KeyEvent.VK_DOWN)) {
 //			playerdy = 0;
-//		}		
+//		}	
 //	}
+
+
+	public void keyPressed(KeyEvent e) {
+		if ((e.getKeyCode() == KeyEvent.VK_W) || (e.getKeyCode() == KeyEvent.VK_UP)) {
+			if (playery > 10) {
+				playery -= 25;	
+			}
+		}
+
+		if ((e.getKeyCode() == KeyEvent.VK_S) || (e.getKeyCode() == KeyEvent.VK_DOWN)) {
+			if (playery < 740) {
+				playery += 25;	
+			}
+		}			
+	}
+
+	public void keyReleased(KeyEvent e) {
+		if ((e.getKeyCode() == KeyEvent.VK_W) || (e.getKeyCode() == KeyEvent.VK_UP) || (e.getKeyCode() == KeyEvent.VK_S) || (e.getKeyCode() == KeyEvent.VK_DOWN)) {
+			playerdy = 0;
+		}		
+	}
 	
 	public int playerx(){return playerx;}
 	public int playery(){return playery;}
